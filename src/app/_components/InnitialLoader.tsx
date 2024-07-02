1c51bd9da55b71302855801fd0a41fa1e17b8f21
NoOne451 <ranjithdurai111@gmail.com>
2024-07-02 20:12:59 +0530

    success

---
+++ src/app/_components/InnitialLoader.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function InitialLoader() {
+  const [show, setShow] = useState(true);
+  //   const body = document?.querySelector('body');
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(false);
+      document?.querySelector('body')?.style.setProperty('position', 'static');
+    }, 800);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!show) return null;
+
+  return (
+    <div className="fixed left-0 right-0 z-[10] w-[100dvw] h-[100dvh] flex items-center justify-center bg-black gap-[5px] uppercase font-extrabold text-[30px]">
+      {/* <div className="logo"></div> */}
+      <p className="text-customGreen zoom-in">Thooku</p>
+      <p className=" text-white zoom-in">Biryani</p>
+    </div>
+  );
+}
