952e407e5e59913c6b95e7b7b006747473ba611c
ash020202 <vimalofficial02@gmail.com>
2024-07-05 10:11:16 +0530

    vimal collab commit

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,12 +1,12 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { clearUser } from '@/Store/Slices/userSlice';
-import { AppDispatch, RootState } from '@/Store/store';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../../backend/Actions/actions';
+"use client";
+import { Button } from "@/components/ui/button";
+import { clearUser } from "@/Store/Slices/userSlice";
+import { AppDispatch, RootState } from "@/Store/store";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../../backend/Actions/actions";
 
 const page = () => {
   const user = useSelector((state: RootState) => state.user);
@@ -20,6 +20,7 @@ const page = () => {
   }
   return (
     <div className="h-dvh w-full bg-black py-[100px] px-[50px] flex justify-center items-center flex-col  gap-5">
+      <h1 className="text-white text-3xl">Collab Working</h1>
       {!user.isAuthenticated && (
         <Link className="text-customGreen" href="/auth/login">
           Login
@@ -39,7 +40,7 @@ const page = () => {
       )}
       {user.isAuthenticated && (
         <Button onClick={handleLogout}>
-          {loading ? 'Logging out...' : 'Logout'}
+          {loading ? "Logging out..." : "Logout"}
         </Button>
       )}
     </div>
