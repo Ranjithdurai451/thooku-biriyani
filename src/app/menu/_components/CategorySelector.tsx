abba9f2d121213775b5218199cf5d8ec5946ab80
ash020202 <vimalofficial02@gmail.com>
2024-07-05 20:33:52 +0530

    account page updated with static data

--- src/app/menu/_components/CategorySelector.tsx
+++ src/app/menu/_components/CategorySelector.tsx
@@ -1,45 +1,45 @@
-'use client';
-import { MenuItemType } from '@/Utils/types';
-import React, { useState } from 'react';
-import MenuItem from './MenuItem';
-import { RootState } from '@/Store/store';
-import { useSelector } from 'react-redux';
+"use client";
+import { MenuItemType } from "@/Utils/types";
+import React, { useState } from "react";
+import MenuItem from "./MenuItem";
+import { RootState } from "@/Store/store";
+import { useSelector } from "react-redux";
 
 const CategorySelector = ({ menuItems }: { menuItems: MenuItemType[] }) => {
-  const [selectedCategory, setSelectedCategory] = useState('combo');
+  const [selectedCategory, setSelectedCategory] = useState("combo");
 
   return (
     <>
       <div className="flex justify-center gap-5 uppercase">
         <button
           className={`px-4 py-2  rounded-full border-customGreen md:text-[16px] text-[14px]   ${
-            selectedCategory === 'combo'
-              ? 'border-2  border-solid text-customGreen'
-              : ''
+            selectedCategory === "combo"
+              ? "border-2  border-solid text-customGreen"
+              : ""
           }`}
-          onClick={() => setSelectedCategory('combo')}
+          onClick={() => setSelectedCategory("combo")}
         >
           Combo&apos;s
         </button>
         <button
           className={`px-4 py-2  rounded-full border-customGreen md:text-[16px] text-[14px]  ${
-            selectedCategory === 'add-on'
-              ? ' border-2  border-solid text-customGreen'
-              : ''
+            selectedCategory === "add-on"
+              ? " border-2  border-solid text-customGreen"
+              : ""
           }`}
-          onClick={() => setSelectedCategory('add-on')}
+          onClick={() => setSelectedCategory("add-on")}
         >
           Add On&apos; s
         </button>
       </div>
-      <div className="grid grid-cols-1 gap-5 xl:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 gap-5 xl:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 slide-in">
         {menuItems &&
           menuItems
             .filter((item) => item.category === selectedCategory)
             .sort((a, b) => {
               return (
-                parseFloat(String(b?.price).replace(/,/g, '')) -
-                parseFloat(String(a?.price).replace(/,/g, ''))
+                parseFloat(String(b?.price).replace(/,/g, "")) -
+                parseFloat(String(a?.price).replace(/,/g, ""))
               );
             })
 
