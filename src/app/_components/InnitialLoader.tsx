62c9ab904b6ee84ffd4ba5107a677baabc8599ba
NoOne451 <126503146+NoOne451@users.noreply.github.com>
2024-07-30 11:57:24 +0530

    Update InnitialLoader.tsx

--- src/app/_components/InnitialLoader.tsx
+++ src/app/_components/InnitialLoader.tsx
@@ -50,7 +50,7 @@ export default function InitialLoader() {
     <div className="fixed left-0 right-0 z-[10] w-[100dvw] h-[100dvh] flex items-center justify-center bg-black gap-[5px] uppercase font-extrabold text-[30px]">
       {/* <div className="logo"></div> */}
       <p className="text-customGreen zoom-in">Thooku</p>
-      <p className=" text-white zoom-in">Biryani</p>
+      <p className=" text-white zoom-in">Biriyani</p>
     </div>
   );
 }
